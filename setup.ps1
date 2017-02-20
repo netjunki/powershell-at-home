c3709ab6503a5849c0bda0d8285dc9016056fc0c
Ben Lau <ben.lau@renaissance.com>
2017-02-20 00:21:02 -0800

    added a script for installing alphafs

---
+++ setup.ps1
@@ -0,0 +1,9 @@
+﻿$url = "https://github.com/alphaleonis/AlphaFS/releases/download/v2.1.2/AlphaFS.2.1.2.0.zip"
+$output = "AlphaFS.2.1.2.0.zip"
+$wc = New-Object System.Net.WebClient
+$wc.DownloadFile($url, "$pwd\$output")
+
+$alphaZip = “$pwd\$output”
+$Destination = “$pwd\alpha”
+Add-Type -assembly “system.io.compression.filesystem”
+[io.compression.zipfile]::ExtractToDirectory($alphaZip, $destination)
