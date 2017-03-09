eb7c12ae0b1da48170dcc51ab15717e5334b7d86
Ben Lau <ben.lau@renaissance.com>
2017-03-09 15:19:22 -0800

    added a script to try to determine what sort of resources something is

---
+++ whatis.ps1
@@ -0,0 +1,43 @@
+﻿param ( 
+  [string]$path
+)
+
+if(-not($path)) { Throw "You must supply a value for -path" }
+
+$libPath = Resolve-Path -Path "$PSScriptRoot\alpha\Lib\Net40\AlphaFS.dll"
+Import-Module -Name $libPath
+$folderPath = Resolve-Path -Path "$path"
+
+Function Invoke-GenericMethod {
+    Param(
+        $Instance,
+        [String]$MethodName,
+        [Type[]]$TypeParameters,
+        [Object[]]$MethodParameters
+    )
+
+    [Collections.ArrayList]$Private:parameterTypes = @{}
+    ForEach ($Private:paramType In $MethodParameters) { [Void]$parameterTypes.Add($paramType.GetType()) }
+
+    $Private:method = $Instance.GetMethod($methodName, "Instance,Static,Public", $Null, $parameterTypes, $Null)
+
+    If ($Null -eq $method) { Throw ('Method: [{0}] not found.' -f ($Instance.ToString() + '.' + $methodName)) }
+    Else {
+        $method = $method.MakeGenericMethod($TypeParameters)
+        $method.Invoke($Instance, $MethodParameters)
+    }
+}
+
+ForEach ($Private:fsei In (Invoke-GenericMethod `
+    -Instance           ([Alphaleonis.Win32.Filesystem.Directory]) `
+    -MethodName         EnumerateFileSystemEntryInfos `
+    -TypeParameters     Alphaleonis.Win32.Filesystem.FileSystemEntryInfo `
+    -MethodParameters   "$folderPath", '*',
+                        ([Alphaleonis.Win32.Filesystem.DirectoryEnumerationOptions]'FilesAndFolders, ContinueOnException'),
+                        ([Alphaleonis.Win32.Filesystem.PathFormat]::FullPath))) {
+     "$fsei"
+     "       Directory: $($fsei.isDirectory)"
+     "      MountPoint: $($fsei.isMountPoint)"
+     "    Reparsepoint: $($fsei.isReparsePoint)"
+     "    SymbolicLink: $($fsei.isSymbolicLink)"
+}
